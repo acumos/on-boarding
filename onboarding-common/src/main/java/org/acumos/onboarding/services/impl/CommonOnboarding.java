77d60c5ccaf80b6755e367a172a6cd7e5eb1b176
Mukhtar <sx00352475@techmahindra.com>
2018-10-11 17:22:26 +0530

    Defect#1830
    Change-Id: I2969735dc195917c8d46ee503ce01a5019a77e71
    Issue-ID: ACUMOS-1839
    Signed-off-by: Mukhtar <sx00352475@techmahindra.com>

--- onboarding-common/src/main/java/org/acumos/onboarding/services/impl/CommonOnboarding.java
+++ onboarding-common/src/main/java/org/acumos/onboarding/services/impl/CommonOnboarding.java
@@ -121,7 +121,6 @@ public class CommonOnboarding {
 
 	protected ResourceUtils resourceUtils;
 	
-	protected String nexusGrpId;
 	
 	@PostConstruct
 	public void init() {
@@ -359,7 +358,7 @@ public class CommonOnboarding {
 		try {
 			FileInputStream fileInputStream = new FileInputStream(file);
 			int size = (int) file.length();
-			nexusGrpId=nexusGroupId+"."+metadata.getSolutionId();
+			String nexusGrpId=nexusGroupId+"."+metadata.getSolutionId();
 			UploadArtifactInfo artifactInfo = artifactClient.uploadArtifact(nexusGrpId, nexusArtifactId, metadata.getVersion(), ext, size, fileInputStream);
 			 
 			logger.debug(EELFLoggerDelegate.debugLogger,
@@ -479,7 +478,8 @@ public class CommonOnboarding {
 			// TODO : Include toscaOutputFolder =/tmp/ and
 			// in external configuration SPRING_APPLICATION_JSON
 			// And define the variable for the same in the class.
-			
+			String nexusGrpId=nexusGroupId+"."+metadata.getSolutionId();
+			logger.debug(EELFLoggerDelegate.debugLogger,"nexusGrpId:" + nexusGrpId);
 			ToscaGeneratorClient client = new ToscaGeneratorClient(toscaOutputFolder, toscaGeneratorEndPointURL,
 					nexusEndPointURL, nexusUserName, nexusPassword, nexusGrpId, cmnDataSvcEndPoinURL, cmnDataSvcUser,
 					cmnDataSvcPwd);
