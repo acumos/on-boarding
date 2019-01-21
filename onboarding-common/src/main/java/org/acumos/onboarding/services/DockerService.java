7ed372ccd15ccdfd027e441b9fd9cb591e3523b0
Jabeen Allauddin Shaikh <JS00353493@TechMahindra.com>
2019-01-21 19:54:51 +0530

    changes regarding interchange formatted model
    Change-Id: I195aaf0a98aa9b047b7ddab97bc2ff534fca7a94
    Issue-ID: ACUMOS-2247,ACUMOS-2242
    Signed-off-by: Jabeen Allauddin Shaikh <JS00353493@TechMahindra.com>

--- onboarding-common/src/main/java/org/acumos/onboarding/services/DockerService.java
+++ onboarding-common/src/main/java/org/acumos/onboarding/services/DockerService.java
@@ -8,9 +8,9 @@
  * under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * This file is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -38,5 +38,9 @@ public interface DockerService {
 	public ResponseEntity<ServiceResponse> onboardModel(HttpServletRequest request, MultipartFile model,
 			MultipartFile metadata, MultipartFile schema, String authorization, String trackingID, String provider,
 			String shareUserName, String modName, Integer deployment_env , String request_id) throws AcumosServiceException;
-	
+
+	ResponseEntity<ServiceResponse> advancedModelOnboard(HttpServletRequest request, MultipartFile model,
+			MultipartFile license, String modName, String authorization, boolean isCreateMicroservice,
+			String dockerfileURL, String provider, String trackingID, String request_id, String shareUserName)
+			throws AcumosServiceException;
 }
