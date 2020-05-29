ceef475b748ff7ab7ae0aa6d8d6afc116c30daa8
PhilippDo <philippe.dooze@orange.com>
2020-05-29 10:33:49 +0200

    Add acumos dependancies in base-r-image
    revert back change 8002 "--no-install-recommends"
    
    Issue-Id: ACUMOS-3861
    Change-Id: Id5389924e575b7326a27292fd5568d1400deaf86
    Signed-off-by: PhilippDo <philippe.dooze@orange.com>
    (cherry picked from commit cdb043d686a404db065a868426dad0bba3a6f35b)

--- base-image/r/acumos.R
+++ base-image/r/acumos.R
@@ -1,1 +1,11 @@
+
+ #install dependancies for acumos package
+
+install.packages("RProtobuf", repos="http://cloud.r-project.org",dependancies=T)
+install.packages("httr", repos="http://cloud.r-project.org",dependancies=T)
+install.packages("jsonlite", repos="http://cloud.r-project.org",dependancies=T)
+install.packages("Rserve", repos="http://cloud.r-project.org",dependancies=T)
+
+#install acumos package in R
+
 install.packages("https://github.com/sambaala/R-acumos/archive/v0.3-0.tar.gz", repos=NULL)
