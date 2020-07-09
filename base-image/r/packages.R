31ec7ec727f8b23197edbd8891b7e38b618ccdd3
PhilippDo <philippe.dooze@orange.com>
2020-07-09 17:25:35 +0200

    Modify onboarding base-r-image
    Isssue-Id: ACUMOS-4218
    Change-Id: I7153a5e81586fc7ee19212ebc17305038f0e358b
    Signed-off-by: PhilippDo <philippe.dooze@orange.com>

---
+++ base-image/r/packages.R
@@ -0,0 +1,10 @@
+install.packages("Rcpp",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("curl",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("RCurl",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("RProtoBuf",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("openssl",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("httr",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("Rserve",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("jsonlite",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("bindr",repos="http://cloud.r-project.org",dependencies=T)
+
