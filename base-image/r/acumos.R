ebe3d5e28960251e57db156d633d0d24ce5088ef
PhilippDo <philippe.dooze@orange.com>
2021-04-07 21:24:59 +0200

    onboarding-base-r not up to date
    Issue-Id: ACUMOS-4325
    Change-Id: Ibc09485d2f1952542105367d02d5136d90d90984
    Signed-off-by: PhilippDo <philippe.dooze@orange.com>

--- base-image/r/acumos.R
+++ base-image/r/acumos.R
@@ -5,7 +5,8 @@ install.packages("RProtoBuf", repos="http://cloud.r-project.org",dependancies=T)
 install.packages("httr", repos="http://cloud.r-project.org",dependancies=T)
 install.packages("jsonlite", repos="http://cloud.r-project.org",dependancies=T)
 install.packages("Rserve", repos="http://cloud.r-project.org",dependancies=T)
+install.packages("remotes",repos="http://cloud.r-project.org",dependencies=T)
 
 #install acumos package in R
 
-install.packages("https://github.com/sambaala/R-acumos/archive/v0.3-0.tar.gz", repos=NULL)
+remotes::install_github("acumos/acumos-r-client", subdir="acumos-package")
