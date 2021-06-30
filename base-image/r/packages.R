cb452a48e15be251843e5c517ee2a0516d6a08e4
Alassane Samba <alassane.samba@orange.com>
2021-06-30 15:17:25 +0200

    Not installing acumos-r-client here for the moment
    Remove installation of acumos-r-client from onboarding base-r-image
    
    ISSUE-ID: ACUMOS-4340
    Signed-off-by: Alassane Samba <alassane.samba@orange.com>
    Change-Id: Ibbe54f8a66f29e05719aaf7dcdfccf9c535f4f32

--- base-image/r/packages.R
+++ base-image/r/packages.R
@@ -1,3 +1,4 @@
+#install dependancies for acumos R packages
 install.packages("Rcpp",repos="http://cloud.r-project.org",dependencies=T)
 install.packages("curl",repos="http://cloud.r-project.org",dependencies=T)
 install.packages("RCurl",repos="http://cloud.r-project.org",dependencies=T)
@@ -7,4 +8,13 @@ install.packages("httr",repos="http://cloud.r-project.org",dependencies=T)
 install.packages("Rserve",repos="http://cloud.r-project.org",dependencies=T)
 install.packages("jsonlite",repos="http://cloud.r-project.org",dependencies=T)
 install.packages("bindr",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("RestRserve",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("yaml",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("remotes",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("bitops",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("R6",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("forecast",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("dplyr",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("lubridate",repos="http://cloud.r-project.org",dependencies=T)
+
 
